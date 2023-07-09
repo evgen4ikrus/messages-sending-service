65dc591f0ffd2dfed173852e68062a67789e955c
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-07-10 02:16:10 +0300

    fix run_mailing script

--- mailings/management/commands/run_mailing.py
+++ mailings/management/commands/run_mailing.py
@@ -1,9 +1,11 @@
+import datetime
 import logging
+import random
 import time
 
 from django.core.management.base import BaseCommand
 
-from mailings.models import Client, Mailing, Message
+from mailings.models import Client, Mailing, Message, Tag, MobileOperatorCode
 
 logger = logging.getLogger('mailing')
 logging.basicConfig(level=logging.INFO)
@@ -12,40 +14,41 @@ logging.basicConfig(level=logging.INFO)
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+
         while True:
 
-            waiting_messages = Message.objects.filter(status='active')
+            for mailing in Mailing.objects.filter(status=''):
+                mailing.status = 'waiting'
+                mailing.save()
+            
+            waiting_mailings = Mailing.objects.filter(status='waiting')
 
-            if not waiting_messages:
-                time.sleep(1)
+            if not waiting_mailings:
                 continue
             
-            for message in waiting_messages:
-                mailing = message.mailing
-                if mailing.status == 'active':
+            for mailing in waiting_mailings:
+                if mailing.ready_to_send:
+                    mailing.status = 'active'
+                    mailing.save()
                     clients = Client.objects.filter(
                         tag__in=mailing.client_tags.all(),
                         mobile_operator_code__in=mailing.client_mobile_operator_codes.all()
                     )
-                    message.status = 'active'
-                    message.save()
-                    message_report = send_messages(message, clients, mailing)
+                    message_report = send_messages(clients, mailing)
                     logger.info(message_report)
-                    message.status = 'completed'
-                    message.save()
+                    mailing.status = 'completed'
+                    mailing.save()
+            time.sleep(1)
 
-
-def send_messages(message, clients, mailing):
-    text = message.text
+def send_messages(clients, mailing):
+    text = mailing.message.text
 
     if not clients:
         return f'Нет подходящих номеров для сообщения: {text}.'
 
     for client in clients:
         mailing = Mailing.objects.get(id=mailing.id)
-        if not mailing.status == 'active':
+        if not mailing.ready_to_send:
             return f'Отправка сообщений {text} прекращена. Сообщения отправлены не всем пользователям.'
-
         logger.info(f'Сообщение:"{text}" отправлено клиенту с номером {client.phone_number}')
-
     return f'Все сообщения: {text} отправлены'
